9fd00c892f36298bf09a65a08aa2eb29647e2810
Rob Tucker <rtucker@mozilla.com>
2012-07-23 12:43:00 -0400

    Updates for pep8

--- MozCef.py
+++ MozCef.py
@@ -6,6 +6,7 @@
 import os
 import syslog
 
+
 class MozCef(object):
 
     def __init__(self, prog_name, facility=syslog.LOG_LOCAL4):
@@ -20,26 +21,26 @@ class MozCef(object):
                 items.append({'cs2': '1234'})
                 items.append({'duser': 'theuser@domain.com'})
                 mc = MozCef('TestApplication')
-                mc.log_cef('Authentication Failed', 
+                mc.log_cef('Authentication Failed',
                             'Authentication failed for user foo', items=items)
             param: prog_name - String to represent the program name in syslog
-            param: facility - syslog facility type, 
+            param: facility - syslog facility type,
                 defaults to syslog.LOG_LOCAL4
         """
         self.prog_name = prog_name
         self.facility = facility
 
-
-    def log_cef(self, message_name, message_description, type='LOG_INFO', items=[]):
+    def log_cef(self, message_name, message_description, type='LOG_INFO',
+                items=[]):
         """
             log_cef method to actually write message to syslog
             param: message_name - Generic name of message type
-            param: message_description - More verbose details of the message to 
+            param: message_description - More verbose details of the message to
                 be logged
-            param: type - A string representation of the syslog facility, 
-                LOG_EMERG, LOG_ALERT, LOG_CRIT, LOG_ERR, LOG_WARNING, 
-                LOG_NOTICE, LOG_INFO, LOG_DEBUG
-                http://unix.superglobalmegacorp.com/Net2/newsrc/sys/syslog.h.html
+            param: type - A string representation of the syslog facility,
+              LOG_EMERG, LOG_ALERT, LOG_CRIT, LOG_ERR, LOG_WARNING,
+              LOG_NOTICE, LOG_INFO, LOG_DEBUG
+              http://unix.superglobalmegacorp.com/Net2/newsrc/sys/syslog.h.html
             param: items - List of cef specific key/value pairs
 
             typical usage:
@@ -53,9 +54,10 @@ class MozCef(object):
         # Iterating over the items list and construct the label string to send
         for row in items:
             for key, value in row.items():
-                label_string = "%s%s=%s " %(label_string, str(key), str(value))
-        cefmsg = 'CEF:0|Mozilla|%s|1.0|%s|%s|5|%s dhost=%s'% (self.prog_name, 
-                message_name, message_description, label_string, os.uname()[1])
+                label_string = "%s%s=%s " % (
+                    label_string, str(key), str(value))
+        cefmsg = 'CEF:0|Mozilla|%s|1.0|%s|%s|5|%s dhost=%s' % (self.prog_name,
+            message_name, message_description, label_string, os.uname()[1])
         try:
             syslog_level = getattr(syslog, type)
         except AttributeError:
